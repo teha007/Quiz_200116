956aa4cdedb63ff6f0f64641e3826b4d178273ea
teha007 <teha007@naver.com>
2020-01-16 18:46:01 +0900

    add

--- app/src/main/java/com/jocom/quiz/DBHelper/DBHelper.java
+++ app/src/main/java/com/jocom/quiz/DBHelper/DBHelper.java
@@ -1,4 +1,27 @@
 package com.jocom.quiz.DBHelper;
 
-public class DBHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+import java.util.List;
+
+public class DBHelper extends SQLiteAssetHelper {
+
+    private static final String DB_NAME = "EDMTQuiz2019.db";
+    private static final int DB_VER = 1;
+
+    private static DBHelper instance;
+
+    public static synchronized DBHelper getInstance(Context context)
+    {
+        if(instance == null)
+            instance = new DBHelper(context);
+        return instance;
+    }
+
+    public DBHelper(Context context) {super(context, DB_NAME, null, DB_VER);}
+
+    public List<Category> getAllCategories()
 }
